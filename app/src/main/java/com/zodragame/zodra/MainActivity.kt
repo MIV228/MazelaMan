aa89a47bb118e748e4aa6a6cdd275a435fa00f81
MIV228 <mivgithub@gmail.com>
2021-05-12 20:25:48 +0700

    commit

--- app/src/main/java/com/zodragame/zodra/MainActivity.kt
+++ app/src/main/java/com/zodragame/zodra/MainActivity.kt
@@ -13,13 +13,13 @@ class MainActivity : AppCompatActivity() {
         var playerItems = {}
 
         fun Items(Name: String, Rarity: String, Usable: Boolean, IsAmmo: Boolean, Ammunition: String, AmmunitionFor: String) {
-        val it_common_zodbox = {Items("common_zodbox", "common", true, false, "", "")}
-        val it_uncommon_zodbox = {Items("uncommon_zodbox", "uncommon", true, false, "","")}
-        val it_rare_zodbox = {Items("rare_zodbox", "rare", true, false, "","")}
-        val it_ultra_rare_zodbox = {Items("ultra_rare_zodbox", "ultra_rare", true, false, "","")}
-        val it_epic_zodbox = {Items("epic_zodbox", "epic", true, false, "","")}
-        val it_glock_17 = {Items("glock-17", "common", true, false, "9mm_bullet","")}
-        val it_9mm_bullet = {Items("9mm_bullet", "uncommon", false, true, "","glock-17")}
+        val item_common_zodbox = {Items("common_zodbox", "common", true, false, "", "")}
+        val item_uncommon_zodbox = {Items("uncommon_zodbox", "uncommon", true, false, "","")}
+        val item_rare_zodbox = {Items("rare_zodbox", "rare", true, false, "","")}
+        val item_ultra_rare_zodbox = {Items("ultra_rare_zodbox", "ultra_rare", true, false, "","")}
+        val item_epic_zodbox = {Items("epic_zodbox", "epic", true, false, "","")}
+        val item_glock_17 = {Items("glock-17", "common", true, false, "9mm_bullet","")}
+        val item_9mm_bullet = {Items("9mm_bullet", "uncommon", false, true, "","glock-17")}
         }
 
         fun OpenZodBox(Rarity: String, DropChance: Int, Items: String, MaxItems: Int, RequiredItems: String, SumOfReqiredItems: Int) {
