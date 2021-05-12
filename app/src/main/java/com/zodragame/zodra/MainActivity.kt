9a6ad94a246ebc852fa87cc86c9e62999cde4176
MIV228 <mivgithub@gmail.com>
2021-05-12 21:10:53 +0700

    commit

--- app/src/main/java/com/zodragame/zodra/MainActivity.kt
+++ app/src/main/java/com/zodragame/zodra/MainActivity.kt
@@ -12,19 +12,19 @@ class MainActivity : AppCompatActivity() {
         var playerShield = 100
         var playerItems = {}
 
-        fun Items(Name: String, Rarity: String, Usable: Boolean, IsAmmo: Boolean, Ammunition: String, AmmunitionFor: String) {
-        val item_common_zodbox = {Items("common_zodbox", "common", true, false, "", "")}
-        val item_uncommon_zodbox = {Items("uncommon_zodbox", "uncommon", true, false, "","")}
-        val item_rare_zodbox = {Items("rare_zodbox", "rare", true, false, "","")}
-        val item_ultra_rare_zodbox = {Items("ultra_rare_zodbox", "ultra_rare", true, false, "","")}
-        val item_epic_zodbox = {Items("epic_zodbox", "epic", true, false, "","")}
-        val item_glock_17 = {Items("glock-17", "common", true, false, "9mm_bullet","")}
-        val item_9mm_bullet = {Items("9mm_bullet", "uncommon", false, true, "","glock-17")}
+        fun funItems(Name: String, Rarity: String, Usable: Boolean, IsAmmo: Boolean, Ammunition: String, AmmunitionFor: String) {
+        val item_common_zodbox = {funItems("common_zodbox", "common", true, false, "", "")}
+        val item_uncommon_zodbox = {funItems("uncommon_zodbox", "uncommon", true, false, "","")}
+        val item_rare_zodbox = {funItems("rare_zodbox", "rare", true, false, "","")}
+        val item_ultra_rare_zodbox = {funItems("ultra_rare_zodbox", "ultra_rare", true, false, "","")}
+        val item_epic_zodbox = {funItems("epic_zodbox", "epic", true, false, "","")}
+        val item_glock_17 = {funItems("glock-17", "common", true, false, "9mm_bullet","")}
+        val item_9mm_bullet = {funItems("9mm_bullet", "uncommon", false, true, "","glock-17")}
         }
 
         fun OpenZodBox(Rarity: String, DropChance: Int, Items: String, MaxItems: Int, RequiredItems: String, SumOfReqiredItems: Int) {
             if (Rarity == "Common") {
-                val MaxItems = 1
+                val Max_Items = 1
             }
         }
 
